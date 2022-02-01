47adb0b7c297823b004d2963f5a29a58998b4580
Aguziendu Ugochukwu <ugochukwua@flexipgroup.com>
2022-02-01 06:35:08 +0100

    click icon to change colormode

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -8,12 +8,12 @@ export const DarkModeSwitch = () => {
     <>
       <Flex position="fixed" top="1rem" right="1rem" py="10px" align="stretch" justify="space-between">
         <Box color="yellow.400" mx="20px">
-          <i className="fas fa-sun fa-2x"></i>
+          <i className="fas fa-sun fa-2x" onClick={() => isDark && toggleColorMode()}></i>
         </Box>
 
         <Switch color="teal" size="lg" colorScheme="teal" isChecked={isDark} onChange={toggleColorMode} />
         <Box color="gray.600" mx="20px">
-          <i className="fas fa-moon fa-2x"></i>
+          <i className="fas fa-moon fa-2x" onClick={() => !isDark && toggleColorMode()}></i>
         </Box>
       </Flex>
     </>
