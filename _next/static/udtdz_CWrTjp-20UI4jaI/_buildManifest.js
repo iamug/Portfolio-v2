b4fdc7ffa29c8e98a487ff47437b3a08db246d67
iamug <uaguziendu@gmail.com>
2022-03-08 11:48:27 +0000

    Deploying to build from @ iamug/Portfolio-v2@b976fe7009313ff931fb8c3d520030f7b6ece780 🚀

---
+++ _next/static/udtdz_CWrTjp-20UI4jaI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/906-61a2148e34ebc142.js","static/chunks/pages/index-ad6598254c5022bb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
