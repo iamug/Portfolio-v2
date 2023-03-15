2e28aff20d5a342cf484808f06da1c7f4afcab2d
iamug <uaguziendu@gmail.com>
2023-03-15 15:59:32 +0000

    Deploying to build from @ iamug/Portfolio-v2@3d5d9cc3bbbd103f86615f208031d08274342ac7 🚀

---
+++ _next/static/Xd-JXjzfv6lGRs6X0UhUq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/906-61a2148e34ebc142.js","static/chunks/pages/index-84c7772100457aa8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
